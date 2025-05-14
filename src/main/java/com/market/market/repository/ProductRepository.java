39a0939f5f55ac56db4bc17b7d62c73c4948fa36
Artur Ardai <krkerdo@gmail.com>
2025-05-14 20:02:47 +0300

    deleting tests

--- src/main/java/com/market/market/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.market.market.repository;
-
-import com.market.market.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
